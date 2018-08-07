a2a8b5c121e0d86d452eb54ed3428048614a2350
k-cormack <k.cormack@gmail.com>
2018-08-07 09:22:32 -0600

    init save

---
+++ app.js
@@ -0,0 +1,37 @@
+var fishingCart = [{
+    product: 'Power Bait',
+    quantity: 2,
+    price: 7.5
+  },{
+    product: 'Spin to Win Lures',
+    quantity: 5,
+    price: .95
+  },{
+    product: 'Penn Spinfisher V SSV8500LL Spinning Reel',
+    quantity: 1,
+    price: 145.42
+  },{
+    product: 'Browning Fishing Micro Stalker Rod',
+    quantity: 1,
+    price: 85
+  },{
+    product: 'BucketO\'Worms',
+    quantity: 2,
+    price: 3
+  },{
+    product: 'Tumps Yacht',
+    quantity: 1,
+    price: 4600002.99
+  }]
+  
+  
+  
+  
+  
+  //Calculate the total cost of the cart provided to the checkout function
+  function checkout(cart){ 
+    
+  }
+  
+  
+  console.log(checkout(fishingCart))
